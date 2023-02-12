b548ec2ea6aa2af5a60540928dce68b846dd0a96
Younes Halim <younes.halim@gmail.com>
2023-02-11 21:15:31 -0500

    Initialized calculator TV variables and added typewriter effect for a more dynamic user interface.

--- app/src/main/java/com/calculator/MainActivity.kt
+++ app/src/main/java/com/calculator/MainActivity.kt
@@ -2,10 +2,40 @@ package com.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvExpression: TextView
+    private lateinit var tvResult: TextView
+
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+        GlobalScope.launch(Dispatchers.Main) { typeWriterEffect() }
+    }
+
+    /**
+     * Uses Kotlin Coroutine to simulate typewriter effect
+     * @return Unit
+     */
+    private suspend fun typeWriterEffect() {
+        val caret: TextView = findViewById<TextView?>(R.id.caret).also {
+            it.append("|")
+        }
+        while (true) {
+            caret.visibility =
+                if (caret.visibility == View.INVISIBLE) View.VISIBLE else View.INVISIBLE
+            delay(500)
+        }
+    }
+
+    private fun init() {
+        tvExpression = findViewById(R.id.tvExpression)
+        tvResult = findViewById(R.id.tvResult)
     }
 }
