3bb37a5ae05b967c50a84af9d03cf25a81bab76e
Younes Halim <younes.halim@gmail.com>
2023-02-12 12:36:21 -0500

    Adding functions for computing and clearing expressions, handling digits and deleting numbers, and evaluating results using Rhino JavaScript engine.

--- app/src/main/java/com/calculator/MainActivity.kt
+++ app/src/main/java/com/calculator/MainActivity.kt
@@ -2,15 +2,21 @@ package com.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import kotlinx.coroutines.*
+import org.mozilla.javascript.Context
+import org.mozilla.javascript.Scriptable
 
 class MainActivity : AppCompatActivity() {
     private lateinit var tvExpression: TextView
     private lateinit var tvResult: TextView
-
+    private lateinit var operand: String
+    private val operators: Regex = "[+\\-*/%√÷×]".toRegex()
     @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,8 +48,10 @@ class MainActivity : AppCompatActivity() {
 
     fun onOperator(view: View) {
         tvExpression.also {
-            if (it.text.isEmpty() || it.text.endsWith(".") || onOperatorAdded(it)) return@also
-            it.append((view as Button).text)
+            if (it.text.isEmpty() || onOperatorAdded(it)) {
+                return@also
+            }
+            it.append((view as Button).text).apply { operand = this.toString() }
         }
     }
 
@@ -59,12 +67,53 @@ class MainActivity : AppCompatActivity() {
                 || expression.text.endsWith("÷")
                 || expression.text.endsWith("×")
                 || expression.text.endsWith("√")
+                || expression.text.endsWith(".")
     }
-    fun compute(view: View) {}
+    fun compute(view: View) {
+        tvExpression.apply {
+            tvExpression.text = tvResult.text
+        }
+    }
+    private fun results () {
+        val context: Context = Context.enter()
+        context.optimizationLevel = -1
+        val scope: Scriptable = context.initStandardObjects()
+        val result: Any = context.evaluateString(scope, tvExpression.text.toString(), "Javascript", 1, null)
+        Context.exit()
+        if (result.toString().isNotEmpty()) {
+            tvResult.text = result.toString()
+            return
+        }
+        tvResult.text = ""
+    }
     fun clear(view: View) {
         tvExpression.text = "".also { tvResult.text = "" }
     }
     fun onDigit(view: View) {
-        tvExpression.also { it.append((view as Button).text) }
+        tvExpression.also {
+            it.append((view as Button).text)
+        }
+        if (tvExpression.text.contains(operators)) {
+            results()
+        } else return
     }
+
+    /**
+     * Clears the calculator TV number by number
+     * @author Younes Halim
+     */
+    fun delete(view: View) {
+        tvExpression.apply {
+            if (this.text.isEmpty()) return@apply
+            val expressionSize = this.text.trim().length
+            var counter = 0
+            counter++
+            if (expressionSize >= counter) {
+                this.text = this.text.substring(0, expressionSize - counter)
+                if (onOperatorAdded(this)) return@apply
+                results()
+            }
+        }
+    }
+
 }
