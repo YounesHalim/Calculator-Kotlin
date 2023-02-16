5e0fc73e53ece67fef339e938a5e97255335c6b4
Younes Halim <younes.halim@gmail.com>
2023-02-15 19:59:57 -0500

    Refactored PreferenceHandlerController class for better code readability and performance

---
+++ app/src/main/java/com/calculator/controller/PreferenceHandlerController.kt
@@ -0,0 +1,41 @@
+package com.calculator.controller
+import android.content.Context
+import android.content.SharedPreferences
+import com.calculator.model.HistoricalData
+
+class PreferenceHandlerController(context: Context) {
+    private val preferences: SharedPreferences = context.getSharedPreferences("my_pref", Context.MODE_PRIVATE)
+
+    /**
+     * Takes History object and saves it in shared preferences
+     * @author Younes Halim
+     * @param HistoricalData
+     */
+    fun saveComputedExpressions(history: HistoricalData) {
+        with(preferences.edit()) {
+            putString(history.dateOfExecution, "${history.mathematicalExpression};${history.result}")
+            this.apply()
+        }
+    }
+    /**
+     * Returns MutableMap of shared preferences data
+     * @author Younes Halim
+     * @return MutableMap
+     */
+    fun getSharedPreferencesDataAsMutableMap(): MutableMap<String, *> {
+        val size = preferences.all.size
+        return if(size == 0) mutableMapOf("1" to "Empty") else preferences.all
+    }
+
+    /**
+     * Clears all data in shared preferences
+     * @author Younes Halim
+     */
+    fun clearHistory() {
+        val size = preferences.all.size
+        if(size == 0) return
+        preferences.edit().apply {
+            this.clear().apply()
+        }
+    }
+}
